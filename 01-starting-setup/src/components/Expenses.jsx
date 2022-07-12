40c35bb9b82ab29b73988f4404d5006c73cee13f
spv89 <sprios.vigos@gmail.com>
2022-07-12 21:44:43 +0200

    Composition(children props) -- Card

---
+++ 01-starting-setup/src/components/Expenses.jsx
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
