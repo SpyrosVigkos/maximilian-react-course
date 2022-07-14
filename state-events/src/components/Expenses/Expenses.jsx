f0a2263f66f7082085b1545ffd1bde48f60c10db
spv89 <sprios.vigos@gmail.com>
2022-07-14 20:45:35 +0200

    Assignment Solution

--- state-events/src/components/Expenses/Expenses.jsx
+++ state-events/src/components/Expenses/Expenses.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const onChnageFilterHandler = (selecetedValue) => {
+    // console.log(selecetedValue);
+    setFilteredYear(selecetedValue);
+  };
+  // console.log(filteredYear);
   return (
     <Card className="expenses">
+      <ExpensesFilter onChangeFilter={onChnageFilterHandler} />
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
