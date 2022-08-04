df113e29708cf6e3b0f72cae271f2b5190701966
spv89 <sprios.vigos@gmail.com>
2022-08-04 16:37:34 +0200

    Spyros Solution

--- forms-user-inputs/src/components/SimpleInput.js
+++ forms-user-inputs/src/components/SimpleInput.js
@@ -2,20 +2,30 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredMail, setEnteredMail] = useState("");
+
   const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+  const [enteredMailIsTouched, setEnteredMailIsTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
 
+  const enteredMailIsValid =
+    /\S+@\S+\.\S+/.test(enteredMail) && enteredMail.trim() !== "";
+  const mailInputIsInvalid = !enteredMailIsValid && enteredMailIsTouched;
+
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredMailIsValid) {
     formIsValid = true;
   }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+  };
+
+  const mailInputChangeHandler = (event) => {
+    setEnteredMail(event.target.value);
   };
 
   const nameInputBlurHandler = (event) => {
@@ -24,20 +34,30 @@ const SimpleInput = (props) => {
       return;
     }
   };
+  const mailInputBlurHandler = (event) => {
+    setEnteredMailIsTouched(true);
+    if (!mailInputIsInvalid) {
+      return;
+    }
+  };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     setEnteredNameIsTouched(true);
-    if (!enteredNameIsValid) {
+    setEnteredMailIsTouched(true);
+    if (!enteredNameIsValid || !enteredMailIsValid) {
       return;
     }
     setEnteredNameIsTouched(false);
+    setEnteredMailIsTouched(false);
     setEnteredName("");
+    setEnteredMail("");
   };
 
-  const nameInputClasses = !nameInputIsInvalid
-    ? "form-control"
-    : "form-control invalid";
+  const nameInputClasses =
+    !nameInputIsInvalid || !nameInputIsInvalid
+      ? "form-control"
+      : "form-control invalid";
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
@@ -52,9 +72,20 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
         )}
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={mailInputChangeHandler}
+          onBlur={mailInputBlurHandler}
+          value={enteredMail}
+        />
+        {mailInputIsInvalid && (
+          <p className="error-text">Name must not be empty and without @.</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
