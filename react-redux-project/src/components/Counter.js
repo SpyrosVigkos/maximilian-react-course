30b14e8efe0077ecbc0e2cce0b74adc05d1023cc
spv89 <sprios.vigos@gmail.com>
2022-08-08 14:09:18 +0200

    init reducx

---
+++ react-redux-project/src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
