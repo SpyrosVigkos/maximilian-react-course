42617fdbcbbee0c16365075a6ddefb18cad5df5b
spv89 <sprios.vigos@gmail.com>
2022-08-09 22:45:02 +0200

    useHistory Hook Feature

--- router-project/src/pages/NewQuote.js
+++ router-project/src/pages/NewQuote.js
@@ -1,7 +1,11 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteDate) => {
     console.log(quoteDate);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
