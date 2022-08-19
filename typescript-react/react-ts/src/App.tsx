709b06bf261dca355b1f3bd79170354a12e560b1
spv89 <sprios.vigos@gmail.com>
2022-08-19 10:36:49 +0200

    add componets

--- typescript-react/react-ts/src/App.tsx
+++ typescript-react/react-ts/src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import Todos from "./componets/Todos";
+import Todo from "./models/todo";
+
 function App() {
+  const todos = [
+    new Todo("Learn React"),
+    new Todo("Learn TS"),
+    new Todo("Learn JS"),
+  ];
   return (
     <div>
-      <Todos items={["Learn React", "Learn TS", "Learn JS"]} />
+      <Todos items={todos} />
     </div>
   );
 }
