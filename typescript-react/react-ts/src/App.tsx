88e067a2ec251b18fe171da4705a4e3a6fd92ad8
spv89 <sprios.vigos@gmail.com>
2022-08-19 14:26:14 +0200

    context api & typescript

--- typescript-react/react-ts/src/App.tsx
+++ typescript-react/react-ts/src/App.tsx
@@ -1,27 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import NewTodo from "./componets/NewTodo";
 import Todos from "./componets/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
